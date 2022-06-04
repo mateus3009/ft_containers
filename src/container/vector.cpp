fea8346149de37df4127ee6059a8a63a5405174d
Mateus Sales Albino <mateus3009@gmail.com>
2022-06-04 17:24:44 -0300

    vector: testing methods

--- src/container/vector.cpp
+++ src/container/vector.cpp
@@ -57,4 +57,63 @@ TEST(vector, constructor_with_size_and_value_and_allocator)
         ASSERT(v[index] == 42);
 }
 
+TEST(vector, max_size)
+{
+    std::allocator<int> alloc;
+
+    NS::vector<int, std::allocator<int> > v(alloc);
+
+    ASSERT(v.max_size() == alloc.max_size());
+}
+
+TEST(vector, size)
+{
+    NS::vector<int> v;
+
+    ASSERT(v.size() == 0);
+}
+
+TEST(vector, size_full)
+{
+    NS::vector<int> v(10);
+
+    ASSERT(v.size() == 10);
+}
+
+TEST(vector, capacity)
+{
+    NS::vector<int> v;
+
+    ASSERT(v.capacity() == 0);
+}
+
+TEST(vector, capacity_full)
+{
+    NS::vector<int> v(10);
+
+    ASSERT(v.capacity() == 10);
+}
+
+TEST(vector, empty)
+{
+    NS::vector<int> v;
+
+    ASSERT(v.empty());
+}
+
+TEST(vector, empty_full)
+{
+    NS::vector<int> v(10);
+
+    ASSERT(!v.empty());
+}
+
+TEST(vector, operator_bracket)
+{
+    NS::vector<int> v(10, 42);
+
+    for (int index = 0; index < 10; ++index)
+        ASSERT(v[index] == 42);
+}
+
 
